3623d65e4d4455c7d141f244228e4576316770ec
Vimal Patel <94vimalpatel@gmail.com>
2020-07-07 02:59:01 +0530

    add comment list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CommentBox from './CommentBox'
+import CommentList from './CommentList'
 import '../App.css'
 
 function App () {
   return (
     <>
       <CommentBox />
+      <CommentList />
     </>
   )
 }
---
+++ src/components/CommentList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CommentList () {
+  return (
+    <div id='comment_list'>
+      <h2>Comment List</h2>
+      <div id='comments'>
+        <p>This is comment #1</p>
+        <p>This is comment #2</p>
+      </div>
+    </div>
+  )
+}
+
+export default CommentList
