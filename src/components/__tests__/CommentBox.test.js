5b06620af03647fa0313f51dabdd9f4bb3ca5944
Vimal Patel <94vimalpatel@gmail.com>
2020-07-07 01:23:51 +0530

    test: ensure textarea clears on submit click

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -28,3 +28,15 @@ it('updates textarea area value based on user input', () => {
   wrapper.update()
   expect(wrapper.find('textarea').prop('value')).toEqual('This is comment #1')
 })
+
+it('clears the textarea on submit button click', () => {
+  expect(wrapper.find('textarea').prop('value')).toEqual('')
+  wrapper
+    .find('textarea')
+    .simulate('change', { target: { value: 'This is comment #1' } })
+  wrapper.update()
+  expect(wrapper.find('textarea').prop('value')).toEqual('This is comment #1')
+  wrapper.find('button').simulate('click')
+  wrapper.update()
+  expect(wrapper.find('textarea').prop('value')).toEqual('')
+})
