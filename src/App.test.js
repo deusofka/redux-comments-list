2b3dd5a8f4ffe6fa4385c59b752e50ef4f368a89
Vimal Patel <94vimalpatel@gmail.com>
2020-07-06 23:32:36 +0530

    refactor: rewrite test with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { shallow } from 'enzyme'
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+let wrapper
+
+beforeEach(() => {
+  wrapper = shallow(<App />)
+})
+
+it('renders', () => {
+  expect(wrapper.exists()).toBe(true)
 })
