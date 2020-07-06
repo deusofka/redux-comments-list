8eb91606c199deaa9126b3f6a9c6bc2d1322cb9e
Vimal Patel <94vimalpatel@gmail.com>
2020-07-07 00:58:59 +0530

    test: ensure comment box has a textarea and button

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -5,9 +5,17 @@ import CommentBox from '../CommentBox'
 let wrapper
 
 beforeEach(() => {
-  wrapper = shallow(<App />)
+  wrapper = shallow(<CommentBox />)
 })
 
 it('renders', () => {
   expect(wrapper.exists()).toBe(true)
+})
+
+it('has a text area', () => {
+  expect(wrapper.find('textarea').length).toEqual(1)
+})
+
+it('has a button', () => {
+  expect(wrapper.find('button').length).toEqual(1)
 })
