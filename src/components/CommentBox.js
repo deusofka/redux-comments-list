fb748ee90de5a2619e7d5dc5f1f84caa5c8c4c78
Vimal Patel <94vimalpatel@gmail.com>
2020-07-07 01:04:38 +0530

    feat: maintain textarea state and handle change

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CommentBox () {
+  const [input, setInput] = useState('')
+
+  function handleChange (event) {
+    setInput(event.target.value)
+  }
+
   return (
     <div id='comment_box'>
       <h2>Enter your comment</h2>
-      <textarea id='input' name='input' id='' cols='30' rows='10' />
+      <textarea
+        id='input'
+        value={input}
+        onChange={handleChange}
+        name='input'
+        cols='30'
+        rows='10'
+      />
       <button>Save Comment</button>
     </div>
   )
