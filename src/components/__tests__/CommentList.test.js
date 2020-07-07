235fc2b3ec3099c27483f836f8ad9f40427713c4
Vimal Patel <94vimalpatel@gmail.com>
2020-07-07 14:44:27 +0530

    test: ensure the comments are shown

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -17,3 +17,20 @@ it('renders', () => {
 it('displays same number of comments as in prop value', () => {
   expect(wrapper.find('p').length).toEqual(2)
 })
+
+it('displays each comment from array prop', () => {
+  // 1. enzyme does not recommend using text(), therefore use render().text()
+  // 2. render() returns a CheerioWrapper around the HTML of the node's subtree
+  expect(
+    wrapper
+      .find('#comments')
+      .render()
+      .text()
+  ).toContain('This is comment #1')
+  expect(
+    wrapper
+      .find('#comments')
+      .render()
+      .text()
+  ).toContain('This is comment #2')
+})
