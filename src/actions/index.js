709db9a63d1845aabfdb8e45ece2f3bddc8d973a
Vimal Patel <94vimalpatel@gmail.com>
2020-07-09 00:02:23 +0530

    feat: add fetch comments action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,17 @@
-import { SAVE_COMMENT } from './types'
+import axios from 'axios'
+import { SAVE_COMMENT, FETCH_COMMENTS } from './types'
 export function saveComment (comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  }
+}
+export function fetchComments () {
+  return async function (dispatch) {
+    const result = await axios('https://jsonplaceholder.typicode.com/comments')
+    dispatch({
+      type: FETCH_COMMENTS,
+      payload: result.data.map(comment => comment.name)
+    })
   }
 }
