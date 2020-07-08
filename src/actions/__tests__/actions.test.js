974d5174b1d4777895e6cf3ea37ecd9ab0f62311
Vimal Patel <94vimalpatel@gmail.com>
2020-07-08 18:02:37 +0530

    test: ensure save comment action payload is right
    Payload should equal the new comment text

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -7,3 +7,12 @@ describe('Save comment', () => {
     expect(saveComment('Live long and prosper').type).toEqual('SAVE_COMMENT')
   })
 })
+
+describe('Save comment', () => {
+  it('has new comment text as payload', () => {
+    expect(saveComment().payload).toEqual(undefined)
+    expect(saveComment('Live long and prosper').payload).toEqual(
+      'Live long and prosper'
+    )
+  })
+})
