9aa45c3f953e2e9eb0d7aff623642d26822d7415
Vimal Patel <94vimalpatel@gmail.com>
2020-07-08 13:47:39 +0530

    feat: add store

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducers from './reducers'
+const initialState = {}
+
+export default createStore(reducers, initialState, composeWithDevTools())
