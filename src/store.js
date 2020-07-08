7835979349efbadc8e131940e2ffcec195ae22df
Vimal Patel <94vimalpatel@gmail.com>
2020-07-09 00:04:24 +0530

    feat: use redux middleware to allow async action

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers'
+import thunk from 'redux-thunk'
 const initialState = { comments: ['This is comment #1', 'This is comment #2'] }
+const middlewares = applyMiddleware(thunk)
 
-export default createStore(reducers, initialState, composeWithDevTools())
+export default createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(middlewares)
+)
