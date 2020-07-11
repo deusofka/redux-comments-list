a23abc4278fa0c498a57400b2b9f404507df2fec
Vimal Patel <94vimalpatel@gmail.com>
2020-07-11 22:58:31 +0530

    feat: show error component for invalid urls

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import CommentBox from './CommentBox'
 import CommentList from './CommentList'
+import Error from './Error'
 import '../App.css'
 
 function App () {
   return (
-    <>
+    <Switch>
       <Route exact path='/' component={CommentList} />
       <Route path='/post' component={CommentBox} />
-    </>
+      <Route component={Error} />
+    </Switch>
   )
 }
 
