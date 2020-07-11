b971049f62883a0477d94f1dfd0952b7f359b605
Vimal Patel <94vimalpatel@gmail.com>
2020-07-11 23:26:55 +0530

    feat: toggle auth on sign in/sign out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { toggleIsAuthenticated } from '../actions'
 import CommentList from './CommentList'
 import CommentBox from './CommentBox'
-const Navbar = () => {
+
+const Navbar = ({ toggleIsAuthenticated, isAuthenticated }) => {
   return (
     <div>
       <Link to='/'>Home</Link>
       <Link to='/post'>Post</Link>
-      <button>Sign In</button>
+      <button onClick={() => toggleIsAuthenticated()}>
+        Sign {!isAuthenticated ? 'In' : 'Out'}
+      </button>
     </div>
   )
 }
 
-export default Navbar
+const mapStateToProps = state => ({
+  isAuthenticated: state.isAuthenticated
+})
+
+export default connect(mapStateToProps, { toggleIsAuthenticated })(Navbar)
