c523fcf639ebb50462f7d8a4e9316ff0bea796e6
Vimal Patel <94vimalpatel@gmail.com>
2020-07-11 20:08:26 +0530

    feat: create auth reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,9 @@
+import TOGGLE_IS_AUTHENTICATED from '../actions/types'
+export default function (state = false, action) {
+  switch (action.type) {
+    case TOGGLE_IS_AUTHENTICATED:
+      return !state
+    default:
+      return state
+  }
+}
