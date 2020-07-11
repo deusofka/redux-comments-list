6940a47819e50bf4786886d8e8244b479260c255
Vimal Patel <94vimalpatel@gmail.com>
2020-07-11 20:17:51 +0530

    feat: combine auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
 import commentReducer from './comments'
+import authReducer from './auth'
 
-export default combineReducers({ comments: commentReducer })
+export default combineReducers({ comments: commentReducer, auth: authReducer })
