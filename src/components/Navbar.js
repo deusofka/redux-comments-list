34d03fc88ae9e43340995761c59c02f998654fbb
Vimal Patel <94vimalpatel@gmail.com>
2020-07-12 01:01:00 +0530

    feat: wrap navbar elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,16 @@ import CommentBox from './CommentBox'
 
 const Navbar = ({ toggleIsAuthenticated, isAuthenticated }) => {
   return (
-    <div>
-      <Link to='/'>Home</Link>
-      <Link to='/post'>Post</Link>
-      <button onClick={() => toggleIsAuthenticated()}>
-        Sign {!isAuthenticated ? 'In' : 'Out'}
-      </button>
+    <div id='navbar'>
+      <div class='horizontal-box'>
+        <div id='navbar_links'>
+          <Link to='/'>Home</Link>
+          <Link to='/post'>Post</Link>
+        </div>
+        <button onClick={() => toggleIsAuthenticated()}>
+          Sign {!isAuthenticated ? 'In' : 'Out'}
+        </button>
+      </div>
     </div>
   )
 }
