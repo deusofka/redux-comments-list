d592673689f499fa1e5700ddb0f1828bc63a9a41
Vimal Patel <94vimalpatel@gmail.com>
2020-07-11 20:15:34 +0530

    fix: use named import syntax to import action type

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,4 +1,4 @@
-import TOGGLE_IS_AUTHENTICATED from '../actions/types'
+import { TOGGLE_IS_AUTHENTICATED } from '../actions/types'
 export default function (state = false, action) {
   switch (action.type) {
     case TOGGLE_IS_AUTHENTICATED:
