a83b47a9af28bc1a1878dafa28da257c0d552758
Vimal Patel <94vimalpatel@gmail.com>
2020-07-13 00:15:43 +0530

    feat: alert when user is unauthorized

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { showAlert } from '../actions'
 function requireAuth (ChildComponent) {
-  function ComposedComponent ({ isAuthenticated, ...rest }) {
+  function ComposedComponent ({ showAlert, isAuthenticated, ...rest }) {
+    useEffect(() => {
+      if (!isAuthenticated) showAlert('Please sign in to access this page!')
+    }, [])
     return isAuthenticated ? <ChildComponent {...rest} /> : <Redirect to='/' />
   }
   function mapStateToProps (state) {
     return { isAuthenticated: state.isAuthenticated }
   }
-  return connect(mapStateToProps)(ComposedComponent)
+  return connect(mapStateToProps, { showAlert })(ComposedComponent)
 }
 
 export default requireAuth
