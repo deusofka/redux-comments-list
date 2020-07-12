19f1f82c82364a3266c90b5b8fc3296beff335da
Vimal Patel <94vimalpatel@gmail.com>
2020-07-13 00:56:45 +0530

    fix: use correct logic to remove alert

--- src/reducers/alerts.js
+++ src/reducers/alerts.js
@@ -4,8 +4,14 @@ export default function (state = [], action) {
     case SHOW_ALERT:
       return [action.payload, ...state]
     case HIDE_ALERT:
-      return state.shift()
+      return removeFirstItem(state)
     default:
       return state
   }
+}
+
+function removeFirstItem (state) {
+  const newState = [...state]
+  newState.splice(0, 1)
+  return newState
 }
