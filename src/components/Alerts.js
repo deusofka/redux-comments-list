11bfa0631430bbe1f0b11c0aeafe85f83c5eb092
Vimal Patel <94vimalpatel@gmail.com>
2020-07-12 23:52:07 +0530

    feat: add alerts component

---
+++ src/components/Alerts.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Alerts = ({ alerts }) => {
+  return (
+    <div>
+      {alerts.map((alert, index) => (
+        <p key={index}>{alert}</p>
+      ))}
+    </div>
+  )
+}
+
+function mapStateToProps (state) {
+  return {
+    alerts: state.alerts
+  }
+}
+
+export default connect(mapStateToProps)(Alerts)
