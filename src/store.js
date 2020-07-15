1a7fcbafb53a829d8f07d967544e03dd7108490b
Vimal Patel <94vimalpatel@gmail.com>
2020-07-16 02:36:14 +0530

    refactor: rename custom middleware references

--- src/store.js
+++ src/store.js
@@ -2,9 +2,9 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers'
 import thunkMiddleware from 'redux-thunk'
-import customMiddleware from './middlewares/async'
+import promiseMiddleware from './middlewares/promiseMiddleware'
 const initialState = { comments: ['This is comment #1', 'This is comment #2'] }
-const middlewares = applyMiddleware(thunkMiddleware, customMiddleware)
+const middlewares = applyMiddleware(thunkMiddleware, promiseMiddleware)
 
 export default createStore(
   reducers,
