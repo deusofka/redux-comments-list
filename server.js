056298d2b4b6027c7789927216d16ecbed71d921
Vimal Patel <94vimalpatel@gmail.com>
2020-07-21 01:51:40 +0530

    feat: connect to database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3090
+const connectDB = require('./config/db')
+
+// Connect Database
+connectDB()
 
 app.listen(port, function () {
   console.log(`Server started listening to port ${port}`)
