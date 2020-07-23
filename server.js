fa4d686fe54dbf6dc410435849a0cff242e6e7a9
Vimal Patel <94vimalpatel@gmail.com>
2020-07-24 00:44:56 +0530

    feat: use express.json() middleware

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3090
 // Connect Database
 connectDB()
 
+/*
+  Middleware: express.json
+  1. Parses incoming requests with JSON payloads
+  2. Populates req with body object containing this parsed data
+ */
+app.use(express.json())
+
 app.listen(port, function () {
   console.log(`Server started listening to port ${port}`)
 })
