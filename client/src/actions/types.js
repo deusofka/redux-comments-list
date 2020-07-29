3b86cdb9fa7232d60b0bd298e62dec3969ed4c02
Vimal Patel <94vimalpatel@gmail.com>
2020-07-30 00:32:39 +0530

    refactor: separate different action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,9 +1,14 @@
+// Comments
 export const SAVE_COMMENT = 'SAVE_COMMENT'
 export const FETCH_COMMENTS = 'FETCH_COMMENTS'
+
+// Auth
 export const AUTHORIZE = 'AUTHORIZE'
 export const AUTH_ERROR = 'AUTH_ERROR'
 export const SIGN_UP = 'SIGN_UP'
 export const SIGN_UP_ERROR = 'SIGN_UP_ERROR'
 export const LOG_OUT = 'LOG_OUT'
+
+// Alert
 export const SHOW_ALERT = 'SHOW_ALERT'
 export const HIDE_ALERT = 'HIDE_ALERT'
