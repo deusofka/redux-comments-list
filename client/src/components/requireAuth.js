faaf2cb3571693f24e48c6c11b4d31738e68c2ff
Vimal Patel <94vimalpatel@gmail.com>
2020-07-31 05:12:01 +0530

    feat: wait for auth to load

--- client/src/components/requireAuth.js
+++ client/src/components/requireAuth.js
@@ -3,9 +3,14 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { showAlert } from '../actions/alerts'
 function requireAuth (ChildComponent) {
-  function ComposedComponent ({ showAlert, isAuthenticated, ...rest }) {
+  function ComposedComponent ({
+    showAlert,
+    isAuthenticated,
+    loading,
+    ...rest
+  }) {
     useEffect(() => {
-      if (!isAuthenticated) {
+      if (!loading && !isAuthenticated) {
         showAlert('warn', 'Please sign in to access this page!')
       }
       // eslint-disable-next-line
@@ -18,7 +23,8 @@ function requireAuth (ChildComponent) {
   }
   function mapStateToProps (state) {
     return {
-      isAuthenticated: state.auth.isAuthenticated
+      isAuthenticated: state.auth.isAuthenticated,
+      loading: state.auth.loading
     }
   }
   return connect(mapStateToProps, { showAlert })(ComposedComponent)
