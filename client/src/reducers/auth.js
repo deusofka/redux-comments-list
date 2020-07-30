bd35295f3e6ba7aac8e22c0eb0c64618e3748368
Vimal Patel <94vimalpatel@gmail.com>
2020-07-31 05:11:30 +0530

    feat: add loading property to auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -7,22 +7,22 @@ import {
   SIGN_UP_ERROR,
   LOG_OUT
 } from '../actions/types'
-const initialState = { isAuthenticated: false, token: null }
+const initialState = { isAuthenticated: false, token: null, loading: true }
 export default function (state = initialState, action) {
   switch (action.type) {
     case AUTHORIZE:
-      return { isAuthenticated: true, token: action.payload }
+      return { isAuthenticated: true, token: action.payload, loading: false }
     case LOG_IN:
     case SIGN_UP:
       localStorage.setItem('token', action.payload)
-      return { isAuthenticated: true, token: action.payload }
+      return { isAuthenticated: true, token: action.payload, loading: false }
     case AUTH_ERROR:
       localStorage.removeItem('token')
-      return { ...initialState }
+      return { ...initialState, loading: false }
     case LOG_IN_ERROR:
     case SIGN_UP_ERROR:
     case LOG_OUT:
-      return { ...initialState }
+      return { ...initialState, loading: false }
     default:
       return state
   }
