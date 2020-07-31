ce1ac4285cd9b5104086f4e20cdd6655e55f0251
Vimal Patel <94vimalpatel@gmail.com>
2020-08-01 01:37:35 +0530

    fix: update state schema

--- client/src/schema/stateSchema.js
+++ client/src/schema/stateSchema.js
@@ -7,13 +7,22 @@ export default {
   default: {},
   examples: [
     {
-      comments: ['Comment 1', 'Comment 2'],
-      isAuthenticated: true,
-      alerts: ['Please sign in to access this page!']
+      comments: ['This is comment #1', 'This is comment #2'],
+      auth: {
+        isAuthenticated: true,
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWYyNDZmODY3ZDI0NjcwOTYxZTY3MTRkIn0sImlhdCI6MTU5NjIyNDI2NiwiZXhwIjoxNTk2MjI3ODY2fQ.kUKq-Ptc-guzCyWDr2WgE1R9w71Vn8vwEjO2OJFwiuY',
+        loading: false
+      },
+      alerts: [
+        {
+          type: 'success',
+          message: 'You can now post a comment!'
+        }
+      ]
     }
   ],
-  required: ['comments', 'isAuthenticated', 'alerts'],
-  additionalProperties: true,
+  required: ['comments', 'auth', 'alerts'],
   properties: {
     comments: {
       $id: '#/properties/comments',
@@ -21,9 +30,10 @@ export default {
       title: 'The comments schema',
       description: 'An explanation about the purpose of this instance.',
       default: [],
-      examples: [['Comment 1', 'Comment 2']],
+      examples: [['This is comment #1', 'This is comment #2']],
       additionalItems: true,
       items: {
+        $id: '#/properties/comments/items',
         anyOf: [
           {
             $id: '#/properties/comments/items/anyOf/0',
@@ -31,19 +41,55 @@ export default {
             title: 'The first anyOf schema',
             description: 'An explanation about the purpose of this instance.',
             default: '',
-            examples: ['Comment 1', 'Comment 2']
+            examples: ['This is comment #1', 'This is comment #2']
           }
-        ],
-        $id: '#/properties/comments/items'
+        ]
       }
     },
-    isAuthenticated: {
-      $id: '#/properties/isAuthenticated',
-      type: 'boolean',
-      title: 'The isAuthenticated schema',
+    auth: {
+      $id: '#/properties/auth',
+      type: 'object',
+      title: 'The auth schema',
       description: 'An explanation about the purpose of this instance.',
-      default: false,
-      examples: [true]
+      default: {},
+      examples: [
+        {
+          isAuthenticated: true,
+          token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWYyNDZmODY3ZDI0NjcwOTYxZTY3MTRkIn0sImlhdCI6MTU5NjIyNDI2NiwiZXhwIjoxNTk2MjI3ODY2fQ.kUKq-Ptc-guzCyWDr2WgE1R9w71Vn8vwEjO2OJFwiuY',
+          loading: false
+        }
+      ],
+      required: ['isAuthenticated', 'token', 'loading'],
+      properties: {
+        isAuthenticated: {
+          $id: '#/properties/auth/properties/isAuthenticated',
+          type: 'boolean',
+          title: 'The isAuthenticated schema',
+          description: 'An explanation about the purpose of this instance.',
+          default: false,
+          examples: [true]
+        },
+        token: {
+          $id: '#/properties/auth/properties/token',
+          type: 'string',
+          title: 'The token schema',
+          description: 'An explanation about the purpose of this instance.',
+          default: '',
+          examples: [
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWYyNDZmODY3ZDI0NjcwOTYxZTY3MTRkIn0sImlhdCI6MTU5NjIyNDI2NiwiZXhwIjoxNTk2MjI3ODY2fQ.kUKq-Ptc-guzCyWDr2WgE1R9w71Vn8vwEjO2OJFwiuY'
+          ]
+        },
+        loading: {
+          $id: '#/properties/auth/properties/loading',
+          type: 'boolean',
+          title: 'The loading schema',
+          description: 'An explanation about the purpose of this instance.',
+          default: false,
+          examples: [false]
+        }
+      },
+      additionalProperties: true
     },
     alerts: {
       $id: '#/properties/alerts',
@@ -51,21 +97,56 @@ export default {
       title: 'The alerts schema',
       description: 'An explanation about the purpose of this instance.',
       default: [],
-      examples: [['Please sign in to access this page!']],
+      examples: [
+        [
+          {
+            type: 'success',
+            message: 'You can now post a comment!'
+          }
+        ]
+      ],
       additionalItems: true,
       items: {
+        $id: '#/properties/alerts/items',
         anyOf: [
           {
             $id: '#/properties/alerts/items/anyOf/0',
-            type: 'string',
+            type: 'object',
             title: 'The first anyOf schema',
             description: 'An explanation about the purpose of this instance.',
-            default: '',
-            examples: ['Please sign in to access this page!']
+            default: {},
+            examples: [
+              {
+                type: 'success',
+                message: 'You can now post a comment!'
+              }
+            ],
+            required: ['type', 'message'],
+            properties: {
+              type: {
+                $id: '#/properties/alerts/items/anyOf/0/properties/type',
+                type: 'string',
+                title: 'The type schema',
+                description:
+                  'An explanation about the purpose of this instance.',
+                default: '',
+                examples: ['success']
+              },
+              message: {
+                $id: '#/properties/alerts/items/anyOf/0/properties/message',
+                type: 'string',
+                title: 'The message schema',
+                description:
+                  'An explanation about the purpose of this instance.',
+                default: '',
+                examples: ['You can now post a comment!']
+              }
+            },
+            additionalProperties: true
           }
-        ],
-        $id: '#/properties/alerts/items'
+        ]
       }
     }
-  }
+  },
+  additionalProperties: true
 }
