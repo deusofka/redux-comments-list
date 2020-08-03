6afd111d21ede2f1d3836bf58d00b6dad801c5aa
Vimal Patel <94vimalpatel@gmail.com>
2020-08-03 06:27:31 +0530

    feat: remove initial list of comments

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,7 @@ import thunkMiddleware from 'redux-thunk'
 import promiseMiddleware from './middlewares/promiseMiddleware'
 import stateValidationMiddleware from './middlewares/stateValidationMiddleware'
 
-const initialState = { comments: ['This is comment #1', 'This is comment #2'] }
+const initialState = {}
 const middlewares = applyMiddleware(
   thunkMiddleware,
   promiseMiddleware,
