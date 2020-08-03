db35d2de4cd1f0671c53201e48928e36dacd1919
Vimal Patel <94vimalpatel@gmail.com>
2020-08-03 05:51:24 +0530

    feat: save comment to db

--- client/src/actions/comments.js
+++ client/src/actions/comments.js
@@ -1,8 +1,18 @@
+import api from '../utils/api'
 import { SAVE_COMMENT } from './types'
+import { showAlert } from './alerts'
 
 export function saveComment (comment) {
-  return {
-    type: SAVE_COMMENT,
-    payload: comment
+  return async function (dispatch) {
+    try {
+      await api.post('/comments', { comment })
+      dispatch({
+        type: SAVE_COMMENT,
+        payload: comment
+      })
+      dispatch(showAlert('success', 'Comment posted successfully'))
+    } catch (error) {
+      dispatch(showAlert('danger', error.response.data.error))
+    }
   }
 }
